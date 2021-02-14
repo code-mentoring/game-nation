87c8053d8eb8ad4388ebf22e560e63d64e413775
edgul <ed.guloien@gmail.com>
2021-02-13 22:15:16 -0500

    Added test files

---
+++ test-server.js
@@ -0,0 +1,15 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/test-index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
