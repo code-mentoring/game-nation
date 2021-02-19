bc9598f107fd362454b5874eed66f2e23fdc36a6
edgul <ed.guloien@gmail.com>
2021-02-18 21:58:21 -0500

    Added auto-server spinup when calling main.py with debug arg

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tkinter import *
 from MainWindow import *
+import sys
+from subprocess import Popen, PIPE
 
-def main():
+def main(argv : list):
+
+    # only spin up server if in debug mode
+    debugMode = argv.count("debug") > 0
+    if debugMode:
+        p = Popen(['node', 'test-server.js'], stdout=PIPE, stderr=PIPE)
+
+    # create GUI and start main event loop
     root = Tk()
     mainWindow = MainWindow(root)
     root.mainloop()
 
+    # cleanup debug server
+    if debugMode: 
+        p.kill()
 
-main()
 
+if __name__ == "__main__":
+   main(sys.argv[1:])
